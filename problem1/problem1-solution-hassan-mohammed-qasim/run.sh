a7ca0ed7c8650395233f4c9e333b3d8ff379a0d3
HIMK322 <62093865+HIMK322@users.noreply.github.com>
2021-10-17 18:25:21 +0300

    Delete problem1 directory

--- problem1/problem1-solution-hassan-mohammed-qasim/run.sh
+++
@@ -1,4 +0,0 @@
-python ./problem1-solution-hassan-mohammed-qasim.py [apples,oranges,milk,bananas,grapes,onines] [apples,grapes,onines
-python ./problem1-solution-hassan-mohammed-qasim.py [apples,oranges,milk,bananas,grapes,onines] [apples,potatoes,onines
-python ./problem1-solution-hassan-mohammed-qasim.py [potatoes,chicken,meat,onines] [chicken,potatoes,onines
-python ./problem1-solution-hassan-mohammed-qasim.py [eggplant,meat,bananas,grapes,onines] [eggplant,meat,tomatoes
